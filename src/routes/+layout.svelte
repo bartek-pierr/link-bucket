2f74bc205dee5e1f2a9d94aa6c5ae0a358646920
Bartek Frątczak <bartek.fratczak@akcept.eu>
2024-06-11 11:35:37 +0200

    Changes in layouts.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,50 +1,55 @@
 <script>
-  export let data;
+    export let data
 
-  import { Auth } from "@supabase/auth-ui-svelte"
-  import { ThemeSupa } from "@supabase/auth-ui-shared"
-  import { goto, invalidateAll } from "$app/navigation"
-  import "../app.css"
+    import { Auth } from '@supabase/auth-ui-svelte'
+    import { ThemeSupa } from '@supabase/auth-ui-shared'
+    import { goto, invalidateAll } from '$app/navigation'
+    import '../app.css'
 
-  let { supabase, session } = data
-  $: ({ supabase, session } = data)
+    let { supabase, session } = data
+    $: ({ supabase, session } = data)
 
-  supabase.auth.onAuthStateChange(async (event, session) => {
-    if(event === "SIGNED_IN") {
-      invalidateAll()
-    }
+    supabase.auth.onAuthStateChange(async (event, session) => {
+        if (event === 'SIGNED_IN') {
+            invalidateAll()
+        }
 
-    if(event === "SIGNED_OUT") {
-      await goto("login")
-      invalidateAll()
-    }
-  })
+        if (event === 'SIGNED_OUT') {
+            await goto('login')
+            invalidateAll()
+        }
+    })
 </script>
 
 <div class="bg-base-100 justify-between fixed top-0 left-0 right-0">
-  <div class="navbar max-w-3xl mx-auto justify-between">
-    <div>
-      <a href="/" class="btn btn-ghost text-xl">LinkBucket</a>
-      {#if session !== null }
-        <a href="/{session.user.email}" class="btn btn-ghost">My Page</a>
-      {/if}
-    </div>
+    <div class="navbar max-w-3xl mx-auto justify-between">
+        <div>
+            <a href="/" class="btn btn-ghost text-xl">LinkBucket</a>
+            {#if session !== null}
+                <a href="/{session.user.email}" class="btn btn-ghost">My Page</a
+                >
+            {/if}
+        </div>
 
-    <div>
-      {#if session == null}
-      <button
-        on:click={() => {
-          goto("/login");
-        }}>Login</button
-      >
-      
-      {:else}
-      <span class="text-white text-lg ml-2">{session.user.email}</span>
-      <button on:click={async () => { await supabase.auth.signOut()}} class="ml-2 btn">Wyloguj się</button>
-      {/if}
-      
+        <div>
+            {#if session == null}
+                <button
+                    on:click={() => {
+                        goto('/login')
+                    }}>Login</button
+                >
+            {:else}
+                <span class="text-white text-lg ml-2">{session.user.email}</span
+                >
+                <button
+                    on:click={async () => {
+                        await supabase.auth.signOut()
+                    }}
+                    class="ml-6 btn">Wyloguj się</button
+                >
+            {/if}
+        </div>
     </div>
-  </div>
 </div>
 
 <slot></slot>
--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,27 +1,34 @@
-import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from "$env/static/public"
-import { createBrowserClient, isBrowser, parse } from "@supabase/ssr"
-import type { LayoutLoad } from "./$types"
+import {
+    PUBLIC_SUPABASE_URL,
+    PUBLIC_SUPABASE_ANON_KEY
+} from '$env/static/public'
+import { createBrowserClient, isBrowser, parse } from '@supabase/ssr'
+import type { LayoutLoad } from './$types'
 
 export const ssr = false
 
 export const load: LayoutLoad = async ({ fetch, data, depends }: any) => {
     depends('sppabase:auth')
 
-    const supabase = createBrowserClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, {
-        global: {
-            fetch
-        },
-        cookies: {
-            get(key) {
-                if(!isBrowser()) {
-                    return JSON.stringify(data.session)
-                }
+    const supabase = createBrowserClient(
+        PUBLIC_SUPABASE_URL,
+        PUBLIC_SUPABASE_ANON_KEY,
+        {
+            global: {
+                fetch
+            },
+            cookies: {
+                get(key) {
+                    if (!isBrowser()) {
+                        return JSON.stringify(data.session)
+                    }
 
-                const cookie = parse(document.cookie)
-                return cookie[key]
+                    const cookie = parse(document.cookie)
+                    return cookie[key]
+                }
             }
         }
-    })
+    )
 
     const {
         data: { session }
