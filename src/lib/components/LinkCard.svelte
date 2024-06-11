3e9982fb9d3660f6b7a5bbd22352e2724a1a8b30
Bartek Frątczak <bartek.fratczak@akcept.eu>
2024-06-11 11:35:10 +0200

    Create LinkCard component.

---
+++ src/lib/components/LinkCard.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    export let link: {
+        id: number
+        logoClearbitAPI: string
+        name: string
+        description: string
+        url: string
+    }
+</script>
+
+<div>
+    <div class="card bg-slate-700 m-4 shadow-lg shadow-blue-900">
+        <a href={link.url} class="card-body">
+            <img
+                src={link.logoClearbitAPI}
+                alt={link.name}
+                class="w-32 h-32 mx-auto"
+            />
+            <div class="text-center">
+                <h2 class="text-white text-2xl font-bold">{link.name}</h2>
+                <p class="text-sky-400">{link.description}</p>
+            </div>
+        </a>
+    </div>
+</div>
+
+<style>
+</style>
